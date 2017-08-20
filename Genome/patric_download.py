ba9595ed048424dd4ddf1dec6287dd9a601cb193
algaebrown <b101102109@tmu.edu.tw>
2017-08-20 13:24:24 +0800

    add function update downloader

--- Genome/patric_download.py
+++ Genome/patric_download.py
@@ -12,13 +12,22 @@ ftp = ftplib.FTP(url)
 ftp.login(user, password)
 root = "/patric2/genomes"
 
+# path of genome
+mypath = "/home/hermuba/resistanceExp/genome"
 # a function to download from patric
 def downloader(ID):
     
-    local_filename = os.path.join("/home/hermuba/resistanceExp/genome", ID+'.fna')
+    local_filename = os.path.join(mypath, ID+'.fna')
     lf = open(local_filename, "wb")
 
     ftp.cwd(root + '/' + ID)
     ftp.retrbinary("RETR " + ID + ".fna", lf.write, 8*1024)
     lf.close()
 
+# check if we already have that file
+def update(ID):
+    onlyfiles = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
+    if ID+'.fna' in onlyfiles:
+        break
+    else:
+        downloader(ID)
