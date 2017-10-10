48c305f3667aa489520428f62a76f5b4df77cab1
algaebrown <b101102109@tmu.edu.tw>
2017-10-10 11:47:33 +0800

    modify to run all Ecoli genome

--- Genome/card_complete_parse.py
+++ Genome/card_complete_parse.py
@@ -1,21 +1,31 @@
-card_path = "/home/hermuba/resistanceExp/EcoliGenomes/cdhitResult/ec0913_card.txt"
+card_path = '/home/hermuba/data/genePredicted/resGeneTxt'
 
-with open(card_path) as f:
-    all_lines = f.readlines()
-
+with open("/home/hermuba/data/ECgenome.txt") as f:
+    genome_list = f.read().splitlines()
 import pandas as pd
-card_detail = pd.DataFrame(columns = ['ARO', 'ARO_name', 'ARO_group', 'snp', 'cut_off'])
+def detail(p):
+    with open(p) as f:
+        all_lines = f.readlines()
 
-# parsing
-header = all_lines[0].split('\t') # len = 25
-for i in range(1, len(all_lines)):
-    attr = all_lines[i].split('\t')
 
-    gene_header = attr[0].split(' ')[0] # JMUY01000005_592|1438670.3
-    cut_off = attr[5] # strict
-    ARO = attr[10] # can be more than one
-    ARO_name = attr[11] # string
-    snp = attr[13] # can be 'n/a'
-    ARO_group = attr[15]
+    card_detail = pd.DataFrame(columns = ['ARO', 'ARO_name', 'ARO_group', 'snp', 'cut_off'])
 
-    card_detail.loc[gene_header] = pd.Series({'ARO':ARO, 'ARO_name':ARO_name, 'ARO_group':ARO_group, 'snp':snp, 'cut_off': cut_off})
+    # parsing
+    header = all_lines[0].split('\t') # len = 25
+    for i in range(1, len(all_lines)):
+        attr = all_lines[i].split('\t')
+
+        gene_header = attr[0].split(' ')[0] # JMUY01000005_592|1438670.3
+        cut_off = attr[5] # strict
+        ARO = attr[10] # can be more than one
+        ARO_name = attr[11] # string
+        snp = attr[13] # can be 'n/a'
+        ARO_group = attr[15]
+
+        card_detail.loc[gene_header] = pd.Series({'ARO':ARO, 'ARO_name':ARO_name, 'ARO_group':ARO_group, 'snp':snp, 'cut_off': cut_off})
+    return(card_detail)
+
+for genome in genome_list:
+    card_detail = detail(card_path +'/'+ genome + '.txt')
+    card_detail.to_pickle('/home/hermuba/data/genePredicted/card_detail_df/'+genome)
+    print(card_detail.shape)
