4d394a1e00a6d1947a14907c06c3fa03a4414181
algaebrown <b101102109@tmu.edu.tw>
2018-01-05 11:10:11 +0800

    add title to tmacc output

---
+++ Drug/add_title_tmacc.py
@@ -0,0 +1,16 @@
+# readfile
+multiple_sdf = '/home/hermuba/data/drug/all_five_sdf.sdf'
+csv = '/home/hermuba/data/drug/all_five.csv'
+# import pybel
+import pybel
+import pandas as pd
+
+df = pd.read_csv(csv)
+df.shape
+n = []
+for molecule in pybel.readfile("sdf",multiple_sdf):
+    name  = molecule.data['PATRIC_NAME_TAG']
+    n.append(name)
+
+df['chem_name'] = n
+df.to_pickle('/home/hermuba/data/tmacc_df')
