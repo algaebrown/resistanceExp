4178cc6d9c0f506f8f4b8ea32a1adce72723fdee
algaebrown <b101102109@tmu.edu.tw>
2018-02-16 14:37:28 +0800

    prodigal using metagenome mode

--- Genome/prodigal.py
+++ Genome/prodigal.py
@@ -1,26 +1,21 @@
+# a script to run prodigal gene prediction on all the genomes
 import os
 
-genome_path = "/home/hermuba/data/genome-gogo"
-gene_path = "/home/hermuba/data/genePredicted"
-# to run prodigal
+# set path of all genome and path of output `gene_path`
+genome_path = "/home/hermuba/data/genome/"
+gene_path = "/home/hermuba/data0118/predicted_genes/"
+genome_list = "/home/hermuba/data0118/genomeList/all"
 
 def run(ID):
-    print("running prodigal for " + ID) 
-    os.system("prodigal -i "+ genome_path +'/'+ ID + ".fna -o garbage.gbk -a "+ ID+".faa")
-    os.system("mv "+ID+".faa ../genePredicted/"+ID+".faa")
-    os.system("rm garbage.gbk")
-
-# check if we already did it
-def update(ID):
-    os.chdir(gene_path)
-    onlyfiles = [f for f in os.listdir('.') if os.path.isfile(os.path.join('./', f))]
-    if ID+'.faa' in onlyfiles:
-        print("we already have prodical for ", ID)
-    else:
-        run(ID)
+    print("running prodigal for " + ID)
+    os.system("prodigal -i "+ genome_path + ID + ".fna -o "+gene_path+ID+".gbk -a "+gene_path+ID+".faa" + " -p meta")
 
 
-with open("/home/hermuba/data/allgenomelist.txt", "r") as f:
+
+id_list = []
+with open(genome_list, "r") as f:
     for ID in f.readlines():
-        update(ID.replace('/n',''))
-        #update(ID)
+        id_list.append(ID.replace('\n',''))
+from multiprocessing import Pool
+with Pool(5) as p:
+    print(p.map(run, id_list))
--- Genome/run_prodigal.sh
+++
@@ -1,2 +0,0 @@
-cd /home/hermuba/data/genome
-cat /home/hermuba/data0118/genomeList/all | parallel wget ftp://ftp.patricbrc.org/patric2/genomes/{}/{}.fna
