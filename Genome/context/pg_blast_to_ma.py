b24b1f179662b9c74c20f8136ad4314a1c79700b
algaebrown <b101102109@tmu.edu.tw>
2018-08-12 16:13:43 +0800

    from blastp output to pivot table; problesm with 'table can at most have 1600 columns

--- Genome/context/pg_blast_to_ma.py
+++ Genome/context/pg_blast_to_ma.py
@@ -1,43 +1,36 @@
-# input: all blastp outputs from one folder
-# header: in dir /data0118/phylo_profile_refseq/faa this command was executed:
-# for file in *.dmnd; do diamond blastp --query /home/hermuba/data0118/cdhit/Escherichia0.70 --db ${file%.*} --out /home/hermuba/data0118/phylo_profile_refseq/${file%.*} --outfmt 6 qseqid qlen ssequid slen sstart send qstart qend evalue bitscore stitle qtitle --verbose --threads 8; done
 
-'''
-Purpose: to make all blastp into a table containing:
-1. rows = genes from pangenome
-2. columns = refseq prtein faa (each genome)
-3. value =
-(1)*e value* of blastp;
-(2)*gene name* to look back to gff for gene nieghboring
+# input: postgresql blastp_out_max_evalue table, with qseqid, evalue, target genome
+# output:
 
-Potential problems:
-1. two hits in one genome? which e-value/gene to pick?
-2. no hit at all genome
+import psycopg2
+from Genome.context.config import config
 
-'''
-# test filename
-filename = '/home/hermuba/data0118/phylo_profile_refseq/GCF_900492165.1_chr1_protein'
-# 5338 hits, 1673 unique
+params = config()
+#params['database'] = 'hermuba' ##test
 
-# Step 1: read one blastp output file --> {gene: evalue, qseqid}
-def read_blastp_out(f):
-    with open(f) as f_handle:
+table_name = 'blastp_abs'
+in_table = 'blastp_out_max_evalue'
+conn = psycopg2.connect(**params)
+cur = conn.cursor()
 
+cur.execute("""
+SELECT DISTINCT target_genome
 
-        e_dict = {}
-        hit_dict = {}
+FROM {0}
+""".format(in_table))
+row_list = [i[0] for i in cur.fetchall()]
+row_string = ''
 
-        for line in f_handle:
-            attr = line.split('\t')
+for row in row_list:
+    row_string = row_string + '\"'+ row + '\"'+' ' + 'decimal' + ','
 
-            # get gene, evalue, qseqid
-            qseqid = attr[0]
-            evalue = attr[8] # make it float
-            sseqid = attr[2]
+cur.execute("""
+    SELECT *
+    INTO {0}
+    FROM crosstab('select qseqid, target_genome, evalue from {1}')
+    AS {0}(qseqid text, {2})
 
-            e_dict[qseqid] = evalue
-            hit_dict[qseqid] = sseqid
-
-        return(e_dict, hit_dict)
-# Step 2: add to the big tables (of size 10000*20000, is df not suitable?)
-# Step 3: mutual information for the evalue table
+    """.format(table_name, in_table, row_string[:-1]))
+conn.commit()
+cur.close()
+conn.close()
