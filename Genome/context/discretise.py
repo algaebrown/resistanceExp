939eb86699e6ee9cfe6c0aae09cacb3f33e964f5
algaebrown <b101102109@tmu.edu.tw>
2018-09-21 09:17:39 +0800

    change to label; remove quantile

--- Genome/context/discretise.py
+++ Genome/context/discretise.py
@@ -1,47 +1,50 @@
 # input: pivot table
 in_path = '/home/hermuba/data0118/mutual_info/'
-table_name = 'blastp_max_e_value'
+table_name = 'eskape_blastp_out_max_evalue'
 
 in_file = in_path + table_name + '_pivot'
-quantile = in_file + table_name + '_quantile'
-ordinary = in_file + table_name + '_ordinary'
+#quantile = in_path + table_name + '_quantile'
+ordinary = in_path + table_name + '_ordinary_label'
 
 import pandas as pd
+from  multiprocessing import Pool
+
+
 # fillna, discretise using two methods:
 def discrete(df):
-    df.fillna(inplace = True)
+    df.fillna(0, inplace = True)
 
-    q_table = pd.DataFrame(index = df.index, columns = df.columns)
+    #q_table = pd.DataFrame(index = df.index, columns = df.columns)
     c_table = pd.DataFrame(index = df.index, columns = df.columns)
 
-    for i for df.index:
-        q_table.loc[i] = pd.qcut(df.loc[i], 415)
-        c_table.loc[i] = pd.ccut(df.loc[i], 415)
-
-    return(q_table, c_table)
+    for index, rows in df.iterrows():
+        c_table.loc[rows.name] = pd.cut(rows, 415, labels = False)
 
 
 
+    return(c_table)
 
 
 
 
-chunks = pd.read_csv(in_file, header = 0, index_col = 0, chunksize = 5, iterator = True)
+
+
+
+chunks = pd.read_csv(in_file, header = 0, index_col = 0, chunksize = 100, iterator = True)
 no_chunk = 0
 for chunk in chunks:
-    q, c = discrete(chunk)
+    c = discrete(chunk)
 
-    with open(quantile) as f:
 
-        if no_chunk == 0:
-            q.to_csv(f, header = True)
+    if no_chunk == 0:
 
-        else:
-            q.to_csv(f, header = False)
-    with open(ordinary) as f:
-        if no_chunk == 0:
+       # with open(quantile, 'w') as f:
+       #     q.to_csv(f, header = True)
+        with open(ordinary, 'w') as f:
             c.to_csv(f, header = True)
-
-        else:
+    else:
+        #with open(quantile, 'a') as f:
+        #    q.to_csv(f, header = False)
+        with open(ordinary, 'a') as f:
             c.to_csv(f, header = False)
     no_chunk += 1
