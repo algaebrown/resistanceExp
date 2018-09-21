498a025ca8b940f7fb9c15b78e04c01a20c5a018
algaebrown <b101102109@tmu.edu.tw>
2018-09-21 09:20:39 +0800

    run diamond all genomes in a folder

--- Genome/context/run_all_diamond_blastp.sh
+++ Genome/context/run_all_diamond_blastp.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # execute in the folder containing all target genome faa
-for file in *.faa
+# specify argument $1 = folder containing all target genome $2 = output folder
+
+cd $1
+pwd
+
+for file in *.faa.gz;
 do
-    diamond makedb --in $file --db ${file%.*}
+    diamond makedb --in $file -d ${file%.faa.gz}
 done
 
 for file in *.dmnd
-do diamond blastp --query /home/hermuba/data0118/cdhit/Escherichia0.70 --db ${file%.*} --out /home/hermuba/data0118/phylo_profile_refseq/${file%.*} --outfmt 6 qseqid qlen ssequid slen sstart send qstart qend evalue bitscore stitle qtitle --verbose --threads 8
+do diamond blastp --query /home/hermuba/data0118/cdhit/Escherichia0.70 --db ${file%.dmnd} --out $2${file%.dmnd} --outfmt 6 qseqid qlen sseqid slen sstart send qstart qend evalue bitscore stitle qtitle --verbose --threads 8
 done
