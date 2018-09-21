a9b44cb6170f7b5f5b61ba15990885bd190b0668
algaebrown <b101102109@tmu.edu.tw>
2018-09-21 11:21:26 +0800

    change label to uniprot API id mapper identifier

--- Genome/nr_id_mapper.py
+++ Genome/nr_id_mapper.py
@@ -14,24 +14,50 @@ def nr_to_uniprot_id(df):
     # identify types of id
 
     def id_mapper(i):
+
         if len(i) == 6:
-            t = 'pdb'
-        if len(i) == 8:
-            t = 'uniprot'
-        if len(i) == 10:
-            t = 'genbank'
-        if len(i) == 11:
-            t = 'refseq'
-        if len(i) == 14:
-            t = 'refseq'
+            t = 'PDB_ID'
+        elif len(i) == 8:
+            t = 'ID' # uniprot
+        elif len(i) == 10:
+            t = 'genbank' ###what to convert in uniprot??genbank protein id?
+        elif len(i) == 11:
+            t = 'P_REFSEQ_AC' # both refseq
+        elif len(i) == 14:
+            t = 'P_REFSEQ_AC' # both refseq
         if 'pir' in i:
-            t = 'uniparc'
-        return(t21)
+            t = 'PIR' # protein info resource
+        if 'prf' in i:
+            t = 'prf' # prf|accession|name
+        #else:
+        #    print(i)
+        return(t)
 
     df['id_type'] = df['sseqid'].map(id_mapper)
 
-    # modify some kinds of id
-    df['new_id'] = df['sseqid']
-    df.loc[df['id_type']=='pdb']['new_id'] = df.loc[df['id_type']=='pdb']['sseqid'].str[:-2] # ex: 2WCI_A to 2WCI
 
     return(df)
+d = nr_to_uniprot_id(dmnd)
+
+# refseq does not need modify
+
+# PDB
+for index in d.loc[d['id_type'] == 'PDB_ID'].index:
+    pdb_id = d.loc[index, 'sseqid'][:4]
+    d.loc[index, 'query_id'] = pdb_id
+    print(pdb_id)
+
+
+
+for index in d.loc[d['id_type'] == 'ID'].index:
+    uniprot_id = d.loc[index, 'sseqid'][:-2]
+    d.loc[index, 'query_id'] = uniprot_id
+    print(uniprot_id)
+
+# need no change
+no_change = ['P_REFSEQ_AC', 'PIR']
+for index in d.loc[d['id_type'].isin(no_change)].index:
+    d.loc[index, 'query_id'] = d.loc[index, 'sseqid']
+
+
+# genbank and prf: don't know how to use id mapper, probably by search
