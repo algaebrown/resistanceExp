4744658537fcdf7c93438381bd3c45258dc0c8c2
algaebrown <b101102109@tmu.edu.tw>
2018-10-07 08:52:00 +0800

    a script to join goldstandard to tables containing mutual information. in SQL

---
+++ Genome/join_goldstandard.sql
@@ -0,0 +1,5 @@
+CREATE TABLE joined_data AS
+SELECT tf_intersect.gene_one, tf_intersect.gene_two, tf_intersect.goldstandard, eskape_mu.mutual_info, eskape_mu.nrm_mutual
+FROM eskape_mu
+FULL JOIN tf_intersect
+ON (tf_intersect.gene_one = eskape_intersect.gene_one AND tf_intersect.gene_two = eskape_intersect.gene_two) OR (tf_intersect.gene_one = eskape_intersect.gene_two AND tf_intersect.gene_two = eskape_intersect.gene_one)
