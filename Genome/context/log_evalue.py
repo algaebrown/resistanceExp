ffc4d52b2892b69f989742e66982acd56089b653
algaebrown <b101102109@tmu.edu.tw>
2018-10-07 20:08:48 +0800

    evalue can = 1

--- Genome/context/log_evalue.py
+++ Genome/context/log_evalue.py
@@ -1,38 +1,67 @@
 # convert evalue to log evalue
 
 # specify these params
-in_table = 'blastp_out_max_evalue'
+table_name = 'blastp_out_max_evalue'
 
-# import
+# import to connect to db
 import psycopg2
 from Genome.context.config import config
-
 params = config()
-#params['database'] = 'hermuba' ##test
-
 conn = psycopg2.connect(**params)
 cur = conn.cursor()
 
+# use eskape_blastp_out as second
+# eskape second best = 6e-317
+# refseq second best = 3e-316
+from Genome.context.transform_evalue import *
+second = find_second_best('cofunctional', 'eskape_blastp_out_max_evalue')
+
+# a temporary role for root for permission of plpythonu
 cur.execute("""
 SET ROLE mydba
-""") # a temporary role for root for permission of plpythonu
+""")
 
+# create or update function
 cur.execute("""
-CREATE FUNCTION log_evalue (a decimal)
+CREATE OR REPLACE FUNCTION log_evalue (a decimal)
   RETURNS decimal
 AS $$
+if a == 0:
+   return(1)
 if a >= 1:
-    return(0)
-if a <= 0:
-    print(1)
+   return(0)
 else:
     import math
-    return(math.log(a)/(-415))
+    return(math.log(a)/-({0}))
+
+
 $$ LANGUAGE plpythonu;
 
-""")
+""".format(str(second)))
 
 conn.commit()
 
+# reset role
+cur.execute("""
+RESET ROLE
+""")
+
+# add column if not exist
+cur.execute("""
+ALTER TABLE {0}
+ADD COLUMN trans_evalue decimal
+
+""".format(table_name))
+
+# update the column 'trans_evalue'
+cur.execute("""
+UPDATE {0}
+SET trans_evalue =  log_evalue(evalue)
+
+
+""".format(table_name))
+
+
+conn.commit()
 cur.close()
 conn.close()
