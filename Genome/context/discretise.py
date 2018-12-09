a144c495fcc8a9383ebf37246b7f12583c891dc2
algaebrown <b101102109@tmu.edu.tw>
2018-12-09 22:22:21 +0800

    fix quatile mappint 1 to 0 bug

--- Genome/context/discretise.py
+++ Genome/context/discretise.py
@@ -1,17 +1,16 @@
-# input: pivot table
-in_path = '/home/hermuba/data0118/mutual_info/'
-table_name = 'blastp_out_max_evalue'
 
-in_file = in_path + table_name + '_pivot'
-quantile = in_path + table_name + '_quantile'
-ordinary = in_path + table_name + '_ordinary'
+
+#table_name = 'blastp_out_max_evalue'
+#in_file = in_path + table_name + '_pivot'
+
+
 
 import pandas as pd
 from  multiprocessing import Pool
 
 
 # fillna, discretise using two methods:
-def discrete(df):
+def ordinary(df, dataset):
     df.fillna(0, inplace = True)
     c_table = pd.DataFrame(index = df.index, columns = df.columns)
     for index, rows in df.iterrows():
@@ -19,44 +18,62 @@ def discrete(df):
     return(c_table)
 
 
-def discrete_equal_bulk(df):
+def quantile(df, dataset):
     import numpy as np
     bin_path = '/home/hermuba/data0118/discretize/'
-    if 'eskape' in table_name:
+    if dataset == 'eskape':
         npy = 'eskape_bins.npy'
-    else:
+    if dataset == 'refseq':
         npy = 'refseq_bins.npy'
 
     npy = bin_path + npy
 
     precalculated_bin = np.load(npy)
+    precalculated_bin = np.append(precalculated_bin, 1.1) # add right boarder to make sure 1 will be captured
 
-    q_table = pd.DataFrame(index = df.index, columns = df.columns, labels = list(range(1, len(npy)+ 2)))
+
+
+
+
+    q_table = pd.DataFrame(index = df.index, columns = df.columns)
 
     for index, rows in df.iterrows():
-        q_table.loc[rows.name] = pd.cut(rows, precalculated_bin)
+        q_table.loc[rows.name] = pd.cut(rows, precalculated_bin, labels = list(range(1, len(precalculated_bin))))
 
     return(q_table.fillna(0))
 
 
 
-def run_chunkwise(in_file, discrete_method):
-    #chunks = pd.read_csv(in_file, header = 0, index_col = 0, nrows = 50)
+def run_chunkwise(in_file, discrete_method, dataset):
+    '''
+    Taking transformed e-value table as the input; discretize using two methods
+    input:
+    1. in_file:pivot table at '/home/hermuba/data0118/mutual_info/'
+    2. discrete_method: `ordinary` or `quantile``
+    3. dataset: 'refseq' or 'eskape'; when using discrete_equal_bulk, require different precalculated binning.
+    output: file in same path, add _quantile or _ordinary
+    '''
+    in_path = '/home/hermuba/data0118/mutual_info/'
+
+    # read file as chunk (chunksize = 100)
     chunks = pd.read_csv(in_file, header = 0, index_col = 0, chunksize = 100, iterator = True)
     no_chunk = 0
-    for chunk in chunks:
-        c = discrete_method(chunk)
 
-        outfile = in_path + table_name + str(discrete_method)
+    # run discretise for each chunk
+    for chunk in chunks:
+        print('running chunk ',no_chunk)
+        c = discrete_method(chunk, dataset)
+
+        # write to file chunkwise
+        outfile = in_path + table_name + discrete_method.__name__
+
+        # add header to first line
         if no_chunk == 0:
 
-            #with open(quantile, 'w') as f:
-                #q.to_csv(f, header = True)
             with open(outfile, 'w') as f:
                 c.to_csv(f, header = True)
+        # if not first chunk, no need to add header
         else:
-            #with open(quantile, 'a') as f:
-                #q.to_csv(f, header = False)
             with open(outfile, 'a') as f:
                 c.to_csv(f, header = False)
         no_chunk += 1
