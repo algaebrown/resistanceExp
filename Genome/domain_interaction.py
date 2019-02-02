3280e8f1b0248d9d0bb533d05e746b8ecb95471e
algaebrown <b101102109@tmu.edu.tw>
2019-02-02 18:28:37 +0800

    a file to generate 010101 domain profile

---
+++ Genome/domain_interaction.py
@@ -0,0 +1,27 @@
+import pandas as pd
+df = pd.read_pickle('/home/hermuba/data0118/goldstandard/node_anno_df')
+
+domain = df['domain'].dropna()
+total_set = set()
+for protein in domain:
+    total_set = total_set.union(protein)
+
+# True/False for domain presence abscence
+outfile = '/home/hermuba/data0118/domain_abs'
+
+
+for i, protein in enumerate(domain):
+    domain_df = pd.DataFrame(columns = list(total_set))
+    for each_domain in protein:
+        domain_df.loc[domain.index[i], each_domain] = 1
+        domain_df = domain_df.fillna(0)
+
+    if i == 0:
+        with open(outfile, 'w') as f:
+            domain_df.to_csv(f, header = True)
+    else:
+        with open(outfile, 'a') as f:
+            domain_df.to_csv(f, header = False)
+    print('doing ' + str(i) + ' out of ' + str(len(domain)))
+
+# calculate weight
