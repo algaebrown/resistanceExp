422463727d1f190653115a6170b97aaf46bf8edb
algaebrown <b101102109@tmu.edu.tw>
2019-02-12 19:56:27 +0800

    rewrite card txt parser using pandas; it's a lot easier

--- Genome/annotate_parser/card_complete_parse.py
+++ Genome/annotate_parser/card_complete_parse.py
@@ -1,34 +1,27 @@
 
-card_path = '/home/hermuba/data/genePredicted/resGeneTxt'
-
-with open("/home/hermuba/data/genomeList/ECgenome.txt") as f:
-    genome_list = f.read().splitlines()
 import pandas as pd
-def detail(p):
-    with open(p) as f:
-        all_lines = f.readlines()
+def parse_card(txt_file):
+    '''
+    a function to parse card output
+    input: card RGI output .txt
+    output: dataframe containing all hits
+    '''
+    df = pd.read_csv(txt_file, header = 0, sep = '\t')
+    return(df)
 
+def process_card(df):
+    '''
+    input: row = hit; column = card info
+    ouput: row = each gene; index = gene_id; columns = 'BEST_Hit_ARO', 'ARO', 'ARO_name', 'Best_Hit_ARO_category', 'ARO_category'
+    '''
 
-    card_detail = pd.DataFrame(columns = ['ARO', 'ARO_name', 'ARO_group', 'snp', 'cut_off', 'best_ARO', 'best_identity'])
+    # filter by e-value cause it's the loose mode
 
-    # parsing
-    header = all_lines[0].split('\t') # len = 25
-    for i in range(1, len(all_lines)):
-        attr = all_lines[i].split('\t')
+    df = df.loc[df['Best_Hit_evalue']<0.00001] # e-value cutoff < 10^ -5
 
-        gene_header = attr[0].split(' ')[0] # JMUY01000005_592|1438670.3
-        cut_off = attr[5] # strict
-        ARO = attr[10] # can be more than one
-        best_ARO = attr[8]
-        best_identity = attr[9]
-        ARO_name = attr[11] # string
-        snp = attr[13] # can be 'n/a'
-        ARO_group = attr[15]
+    # split the representing gene id
+    df.set_index(df['ORF_ID'].str.split(' ', expand = True)[0])
 
-        card_detail.loc[gene_header] = pd.Series({'ARO':ARO, 'ARO_name':ARO_name, 'ARO_group':ARO_group, 'snp':snp, 'cut_off': cut_off, 'best_ARO': best_ARO, 'best_identity' : best_identity})
-    return(card_detail)
-
-for genome in genome_list:
-    card_detail = detail(card_path +'/'+ genome + '.txt')
-    card_detail.to_pickle('/home/hermuba/data/genePredicted/card_detail_df/'+genome)
-    print(card_detail.shape)
+    # drop old ORF ID
+    df.drop('ORF_ID', axis = 1)
+    return(df)
