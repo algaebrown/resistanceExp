0d4be94521e0adefce19de831732e39665c0323c
algaebrown <b101102109@tmu.edu.tw>
2019-03-31 16:01:24 +0800

    this script is useless because it does not add genome ID to the faa header

--- Genome/pangenome_build/concat_for_cdhit.sh
+++ Genome/pangenome_build/concat_for_cdhit.sh
@@ -6,11 +6,22 @@ faa_path=/home/hermuba/data/genePredicted/
 
 # an empty file
 touch /home/hermuba/data0118/ec_remove_plasmid_1580.faa
+touch /home/hermuba/data0118/genome_list/ecoli_final
+
+# 
+final_list=/home/hermuba/data0118/genome_list/ecoli_final
 
 # concat at the end of the file
 for genome in `cat $genome_list`
 do
 
     faa=$faa_path$genome.faa
+if [ -e $faa ]
+then
     cat $faa >> /home/hermuba/data0118/ec_remove_plasmid_1580.faa
+    echo $genome >> $final_list
+else
+    echo $genome
+fi
+
 done
