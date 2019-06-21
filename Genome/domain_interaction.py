8a46189bd07e457cbe0e79e3b2a19a31839f18aa
algaebrown <b101102109@tmu.edu.tw>
2019-06-21 20:47:52 +0800

    debug

--- Genome/domain_interaction.py
+++ Genome/domain_interaction.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df = pd.read_pickle('/home/hermuba/data0118/goldstandard/node_anno_df')
+df = pd.read_pickle('/home/hermuba/data0118/goldstandard/ec_rmplasmid_node_anno_df')
 
 domain = df['domain'].dropna()
 total_set = set()
@@ -7,7 +7,7 @@ for protein in domain:
     total_set = total_set.union(protein)
 
 # True/False for domain presence abscence
-outfile = '/home/hermuba/data0118/domain_abs'
+outfile = '/home/hermuba/data0118/domain_abs_rm_plasmid'
 
 
 for i, protein in enumerate(domain):
