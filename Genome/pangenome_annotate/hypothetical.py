1ecb30d6f0663b9e471866f4f14b85681951262f
algaebrown <b101102109@tmu.edu.tw>
2019-09-12 02:14:51 +0800

    update DUF as hypothetical

--- Genome/pangenome_annotate/hypothetical.py
+++ Genome/pangenome_annotate/hypothetical.py
@@ -5,7 +5,7 @@ def identify_hypothetical(df):
     input: dataframe generated from Genome.annotate_parser.parse_blast.parse_diamond()
     output: dataframe with additional column "hypothetical"; True includes either "hypothetical protein, unknown protein, and uncharacterized protein"
     '''
-    hypo = df.loc[df['stitle'].str.contains('hypothetical') | df['stitle'].str.contains('ncharacter') | df['stitle'].str.contains('unknown')]
+    hypo = df.loc[df['stitle'].str.contains('hypothetical') | df['stitle'].str.contains('ncharacter') | df['stitle'].str.contains('unknown') | df['stitle'].str.contains('DUF')]
 
     df.loc[hypo.index, 'hypothetical'] = True
 
