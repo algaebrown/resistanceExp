564f35e484fb59ae7d6bf5beb4ffd5e5365887ce
algaebrown <b101102109@tmu.edu.tw>
2019-09-13 04:11:40 +0800

    fix bug on running scoary with my pangenome

--- compare_scoary/run_scoary.py
+++ compare_scoary/run_scoary.py
@@ -1,48 +1,45 @@
-ris_file = "/home/hermuba/data0118/annotated_RIS/anno_df"
-pan_file = "/home/hermuba/data0118/cdhit/clstr/pangenome_df/Escherichia0.70.clstr.csv"
+ris_file = "/home/hermuba/data0118/annotated_RIS/RIS_pivot_ecoli.csv"
+pan_file = "/home/hermuba/data0118/cdhit/clstr/pangenome_df/Escherichia0.70rm_plasmid.clstr.csv"
 trait_file = '~/data0118/scoary/scoary_trait.csv'
 abs_file = '~/data0118/scoary/gene_abs.Rtab'
 #======generate trait file======
 
 # ris dataframe: select only E.coli
 import pandas as pd
-ris = pd.read_pickle(ris_file)
-ris_need = ris[['Genome ID', 'Antibiotic', 'Resistant Phenotype', 'Measurement Value', 'Species']]
-ris_need = ris_need.loc[ris_need['Species'] == 'Escherichia']
-# change to binary
-to_binary={'Resistant': 1,
-           'Susceptible': 0}
-ris_need['Resistnat Phenotype'] = ris_need['Resistant Phenotype'].map(to_binary)
+pan = pd.read_csv(pan_file, index_col = 0, header = 0)
+ris = pd.read_csv(ris_file, header = 0, dtype = str)
+ris.set_index('Genome ID', inplace = True)
+print("files read")
 
-# construct the row = genome; column = trait table
-genome = ris_need['Genome ID'].unique()
-abx = ris_need['Antibiotic'].unique()
-matrix = pd.DataFrame(columns = abx, index = genome)
-import numpy as np
-for index, row in ris_need.iterrows():
-    if row['Resistant Phenotype'] == "Resistant":
-        matrix.loc[row['Genome ID'], row['Antibiotic']] = 1
-    elif type(row['Resistant Phenotype']) == str:
-        matrix.loc[row['Genome ID'], row['Antibiotic']] = 0
-    else:
-        matrix.loc[row['Genome ID'], row['Antibiotic']] = 'NA'
+# select included genome
+matrix  = ris.loc[ris.index.isin(pan.columns)]
+print("selecting included genome")
+# fill na
+# scoary fails if data = 1
+matrix = matrix.loc[:,matrix.count() > 50]
+matrix.fillna('NA', inplace = True)
+matrix.replace('0.0', '0', inplace = True)
+matrix.replace('1.0', '1', inplace = True)
 
 
 
 
-# save trait file to csv
-matrix.to_csv(trait_file)
+
+
+matrix.to_csv(trait_file, index_label = 'Name')
+print("phenotype pivot table saved")
 
 #======generate pangenome absence presence pattern======
-pan = pd.read_csv(pan_file, index_col = 0, header = 0)
-import numpy as np
-pan = pan.iloc[:-1, :]
-pan = pan.astype(int).replace(0, np.nan)
-pan.to_csv(abs_file, index_label = 'Gene')
 
+import numpy as np
+# remove the last line of nan
+pan = pan.iloc[:-1, :]
+
+pan = pan[matrix.index.tolist()]
+pan = pan.astype(int) # needs to be int
+pan.to_csv(abs_file, index_label = 'Gene')
+print("subset genomes with phenotype data")
 
 #======run scoary======
 import os
-os.system('scoary -g '+ abs_file + ' -t ' + trait_file + ' -s 1 --delimiter ,')
-
-# this script cannot work: pdist: 2 dimensional array required...= =
+os.system('scoary -g '+ abs_file + ' -t ' + trait_file + ' -s 2 --delimiter , --threads 16')
