e49be0042efde0b9aec033d44d2192ce6ac41c93
algaebrown <b101102109@tmu.edu.tw>
2019-09-19 10:10:39 +0800

    remove erroneous assignment of drug calss

--- Drug/abx_annotation.py
+++ Drug/abx_annotation.py
@@ -5,14 +5,62 @@ category = clsi[['category', 'drug']]
 category = category.drop_duplicates(subset = 'drug')
 order = ['penicillins', 'b_lactam_inhibitor', 'cephems', 'monobactams', 'carbapenems', 'lipopeptides', 'phenicols', 'macrolides', 'aminoglycosides', 'tetracyclines', 'quinolones', 'folate_path','nitrofurans', 'fosfomycins', 'licosamide', 'pleuromutilin']
 
-all_drug = ['ertapenem', 'cephalexin', 'clindamycin', 'polymyxin b', 'tilmicosin','tazobactam', 'imipenem', 'cefoperazone', 'sulbactam', 'doripenem','cefazolin', 'piperacillin', 'ceftazidime', 'timentin', 'trimethoprim','tigecycline', 'chloramphenicol', 'ceftriaxone', 'cefuroxime','amikacin', 'levofloxacin', 'sulfisoxazole', 'azithromycin','cefotaxime', 'danofloxacin', 'sulfamethoxazole', 'enrofloxacin','kanamycin', 'colistin', 'ticarcillin', 'doxycycline', 'gentamicin','cefotetan', 'fosfomycin', 'sulfadimethoxine', 'oxytetracycline','minocycline', 'cephalothin', 'ciprofloxacin', 'florfenicol','nalidixic acid', 'ceftiofur', 'clavulanic acid', 'tetracycline','tiamulin', 'neomycin', 'aztreonam', 'ampicillin', 'meropenem','tylosin', 'spectinomycin', 'nitrofurantoin', 'chlortetracycline','cefepime', 'streptomycin', 'cefoxitin', 'amoxicillin', 'tobramycin','norfloxacin', 'moxifloxacin']
+all_drug = ['ertapenem', 'cephalexin', 'clindamycin', 'polymyxin b', 'tilmicosin','tazobactam', 'imipenem', 'cefoperazone', 'sulbactam', 'doripenem','cefazolin', 'piperacillin', 'ceftazidime', 'timentin', 'trimethoprim','tigecycline', 'chloramphenicol', 'ceftriaxone', 'cefuroxime','amikacin', 'levofloxacin', 'sulfisoxazole', 'azithromycin','cefotaxime', 'danofloxacin', 'sulfamethoxazole', 'enrofloxacin','kanamycin', 'colistin', 'ticarcillin', 'doxycycline', 'gentamicin','cefotetan', 'fosfomycin', 'sulfadimethoxine', 'oxytetracycline','minocycline', 'cephalothin', 'ciprofloxacin', 'florfenicol','nalidixic acid', 'ceftiofur', 'clavulanic acid', 'tetracycline','tiamulin', 'neomycin', 'aztreonam', 'ampicillin', 'meropenem','tylosin', 'spectinomycin', 'nitrofurantoin', 'chlortetracycline','cefepime', 'streptomycin', 'cefoxitin', 'amoxicillin', 'tobramycin','norfloxacin', 'moxifloxacin', 'amoxicillin-clavunate', 'trimethoprim-sulfamethoxazole','cefalotin']
 
 # ADD MISSING:
 
 missing = list(set(all_drug)-set(category['drug']))
-cat = ['b_lactam_inhibitor', 'phenicols', 'folate_path', 'cephems', 'penicillins', 'quinolones', 'cephems', 'macrolides', 'macrolides', 'lipopeptides', 'aminoglycosides', 'penicillins', 'tetracyclines', 'aminoglycosides', 'tetracyclines', 'cephems', 'aminoglycosides', 'folate_path', 'b_lactam_inhibitor', 'tetracyclines', 'licosamide', 'quinolones', 'b_lactam_inhibitor', 'b_lactam_inhibitor', 'pleuromutilin', 'quinolones', 'folate_path']
+missing.sort()
+cat = ['penicillins',
+       'b_lactam_inhibitor',
+       'cephems',
+       'cephems',
+       'cephems',
+       'cephems',
+       'tetracyclines',
+       'b_lactam_inhibitor',
+       'licosamide',
+       'quinolones',
+       'quinolones',
+       'phenicols',
+       'quinolones',
+       'aminoglycosides',
+       'tetracyclines',
+       'lipopeptides',
+       'aminoglycosides',
+       'aminoglycosides',
+       'b_lactam_inhibitor',
+       'folate_path',
+       'folate_path',
+       'folate_path',
+       'b_lactam_inhibitor',
+       'pleuromutilin',
+       'penicillins',
+       'tetracyclines',
+       'macrolides',
+       'b_lactam_inhibitor',
+       'folate_path',
+       'macrolides']
+
+
 
 df = pd.DataFrame({'drug' : missing, 'category' :cat})
 category = category.append(df, ignore_index = True)
 for i in order:
     category.loc[category['category'] == i,'order'] = order.index(i)
+
+
+category.set_index('drug', inplace = True)
+
+
+# map a color to each drug
+import seaborn as sns
+lut = dict(zip(category['order'].sort_values().unique(), sns.color_palette("hls", len(category['order'].unique()))))
+category['drug_color'] = category['order'].map(lut)
+
+
+
+# save to file
+category.sort_values(by = 'order', inplace = True)
+
+category.to_pickle('/home/hermuba/data0118/drug_category')
