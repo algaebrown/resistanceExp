e39e5bc3cbd68999d0e632caec6bf1cad8b3e10d
algaebrown <b101102109@tmu.edu.tw>
2020-12-07 09:23:13 +0800

    clean unused file

--- Genome/co-citation/uniprot.py
+++
@@ -1,7 +0,0 @@
-# load blast result
-
-
-# accessing uniprot ID converter throught gi and accession numbero
-from biosevices.uniprot import Uniport
-u = UniProt(verbose=False)
-u.mapping(fr="ACC+ID", to="ID", query='P43403') # to uniprot ID
